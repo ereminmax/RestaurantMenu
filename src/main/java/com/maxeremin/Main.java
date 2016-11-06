2a510f252b50d0ef0997c18ec6c4d35a69c22f14
ereminmax <soloveyj@aol.com>
2016-11-06 23:54:03 +0300

    logger works with the file system also

--- src/main/java/com/maxeremin/Main.java
+++ src/main/java/com/maxeremin/Main.java
@@ -1,10 +1,15 @@
 package com.maxeremin;
 
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 class Main {
 
     Logger logger = Logger.getLogger(Main.class.getName());
+    FileHandler fh;
 
     public static void main(String ... arg) {
 
@@ -14,7 +19,21 @@ class Main {
     }
 
     private void doSomeLog() {
+
+        try {
+
+            fh = new FileHandler("C:\\Git\\NCMentor\\src\\test\\java\\MyLogFile.log");
+            logger.addHandler(fh);
+
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
         logger.info("Greeting starts\n");
+
     }
 
 }
