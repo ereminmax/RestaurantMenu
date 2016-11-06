9de25a562936eb4da6e2a067b48b3e191f2d73ba
ereminmax <soloveyj@aol.com>
2016-11-06 23:17:11 +0300

    logger added

--- src/main/java/com/maxeremin/Main.java
+++ src/main/java/com/maxeremin/Main.java
@@ -1,11 +1,20 @@
 package com.maxeremin;
 
+import java.util.logging.Logger;
+
 class Main {
+
+    Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String ... arg) {
 
+        new Main().doSomeLog();
         System.out.println("Hello, Maven!");
 
+    }
+
+    private void doSomeLog() {
+        logger.info("Greeting starts\n");
     }
 
 }
